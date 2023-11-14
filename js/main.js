89a26cfb2d02cbb7b4381f44ede9b125264ce7b3
aajy <aajy.000@gmail.com>
2023-11-14 23:39:44 +0900

    각 시,분,초가 한자리일때 두자리로 변환하는 함수 추가

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
-const h1 = document.querySelector('h1');
-const [btnStart, btnStop] = document.querySelectorAll('button');
-let num = 0;
-let timer = null;
+const [spanHr, spanMin, spanSec] = document.querySelectorAll('span');
 
-timer = setInterval(() => {
-	h1.innerText = num++;
+setInterval(() => {
+	const hr = new Date().getHours();
+	const min = new Date().getMinutes();
+	const sec = new Date().getSeconds();
+
+	spanHr.innerText = plusZero(1);
+	spanMin.innerText = plusZero(min);
+	spanSec.innerText = plusZero(sec);
 }, 1000);
 
-btnStop.addEventListener('click', () => {
-	clearInterval(timer);
-});
-
-btnStart.addEventListener('click', () => {
-	timer = setInterval(() => {
-		h1.innerText = num++;
-	}, 1000);
-});
+function plusZero(el) {
+	if (String(el).length < 2) {
+		return (el = '0' + el);
+	} else {
+		return el;
+	}
+}
