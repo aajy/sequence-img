8a3f5ce80fa86bd28c5960b4f61681d4790707cd
aajy <aajy.000@gmail.com>
2023-11-14 13:10:00 +0900

    전역 변수와 setInterval

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-class Student {
-	constructor(name, age) {
-		this.name = name;
-		this.age = age;
-		this.school = 'DECODELAB';
-	}
-	plusAge() {
-		this.age = this.age + 1;
-	}
-}
-const s1 = new Student('David', 20);
-const s2 = new Student('Emily', 21);
-console.log('s1: ', s1);
-console.log('s2: ', s2);
-[s1, s2].forEach((el) => el.plusAge());
-console.log('s1: ', s1);
-console.log('s2: ', s2);
+const h1 = document.querySelector('h1');
+const [btnStart, btnStop] = document.querySelectorAll('button');
+let num = 0;
+let timer = null;
+
+timer = setInterval(() => {
+	h1.innerText = num++;
+}, 1000);
+
+btnStop.addEventListener('click', () => {
+	clearInterval(timer);
+});
+
+btnStart.addEventListener('click', () => {
+	timer = setInterval(() => {
+		h1.innerText = num++;
+	}, 1000);
+});
