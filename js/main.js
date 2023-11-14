072a1a0923708f6efdb4392acc148bb9629ebdd3
aajy <aajy.000@gmail.com>
2023-11-14 23:45:58 +0900

    초에 따른 배경색 변화

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const p = document.querySelector('p');
 const [spanHr, spanMin, spanSec] = document.querySelectorAll('span');
 
 setInterval(() => {
@@ -8,6 +9,16 @@ setInterval(() => {
 	spanHr.innerText = checkLength(hr);
 	spanMin.innerText = checkLength(min);
 	spanSec.innerText = checkLength(sec);
+
+	if (0 <= sec && sec < 21) {
+		p.style.backgroundColor = '#333';
+	}
+	if (21 <= sec && sec < 41) {
+		p.style.backgroundColor = '#999';
+	}
+	if (41 <= sec && sec < 61) {
+		p.style.backgroundColor = '#aaa';
+	}
 }, 1000);
 
 function checkLength(el) {
