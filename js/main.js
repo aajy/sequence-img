55529276d56fd2fabcd1fc0345552d1d19a96791
aajy <aajy.000@gmail.com>
2023-11-15 23:37:53 +0900

    12시 초과 시 -12하여 표기하도록 함수 추가

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 const screen = document.querySelector('.screen');
 const em = screen.querySelector('em');
-const [spanHr, spanMin, spanSec] = screen.querySelectorAll('span');
+const numbers = screen.querySelectorAll('span');
 
 setInterval(() => {
-	let now = new Date();
+	em.innerText = new Date().getHours() < 12 ? 'am' : 'pm';
+
+	getTime().forEach((num, idx) => setTime(num, idx));
+}, 1000);
+
+//시간값을 구해서 반환하는 함수
+function getTime() {
+	const now = new Date();
 	let hr = now.getHours();
 	let min = now.getMinutes();
 	let sec = now.getSeconds();
 
-	em.innerText = hr < 12 ? 'em' : 'pm';
+	//현재시간값이 13이상이되면 12를 뺀값을 hr로 리턴
+	hr = hr > 12 ? hr - 12 : hr;
+	return [hr, min, sec];
+}
 
-	spanHr.innerText = hr < 10 ? '0' + hr : hr;
-	spanMin.innerText = min < 10 ? '0' + min : min;
-	spanSec.innerText = sec < 10 ? '0' + sec : sec;
-	console.log(now);
-}, 1000);
+//반환된 시간값을 인수로 받아서 DOM에 세팅하는 함수
+function setTime(num, index) {
+	numbers[index].innerText = num < 10 ? '0' + num : num;
+}
